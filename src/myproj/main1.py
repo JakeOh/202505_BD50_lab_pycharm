9708ca985ae7e33f8ee0c328491bc40ae0a11420
JakeOh <jake.oh.lecture@gmail.com>
2025-06-17 13:30:03 +0900

    initial commit

---
+++ src/myproj/main1.py
@@ -0,0 +1,7 @@
+import sys
+import myutil.plus as my_plus
+
+if __name__ == '__main__':
+    print(sys.version)
+    result = my_plus.plus(1, 2)
+    print('result =', result)
---
+++ src/myproj/myutil/plus.py
@@ -0,0 +1,13 @@
+def plus(x, y):
+    """
+    두 숫자 합 x + y를 리턴.
+    :param x: 숫자(int, float)
+    :param y: 숫자(int, float)
+    :return: x + y
+    """
+    return x + y
+
+# print('plus.py __name__ =', __name__)
+
+if __name__ == '__main__':
+    print(plus(1, 2))
