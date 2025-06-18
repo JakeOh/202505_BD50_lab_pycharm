b96f40711af8774a953f610c58e3eeabfb885ee2
JakeOh <jake.oh.lecture@gmail.com>
2025-06-18 14:17:27 +0900

    oracledb 패키지를 사용한 ddl, dql 모듈 작성

--- src/myproj/db_util/dql.py
+++ src/myproj/db_util/dql.py
@@ -17,5 +17,49 @@ def select_dept():
                 print(e)
 
 
+# 부서번호로 부서 정보 검색 -> 파라미터를 갖는 sql 문장
+sql_select_by_deptno = 'select * from dept_ex where deptno = :dept_no'
+
+# sql_select_by_deptno 문장을 실행하고 결과를 출력하는 함수
+def select_dept_by_deptno(no):
+    with get_connection() as conn:
+        with conn.cursor() as cursor:
+            try:
+                # 가변길이 키워드 아규먼트 방식으로 SQL 문장에 값을 설정하는 방법.
+                cursor.execute(sql_select_by_deptno, dept_no=no)
+                for row in cursor:
+                    print(row)
+            except DatabaseError as e:
+                print(e)
+
+
+# 부서 이름에 포함되는 문자열로 대/소문자 구분없이 검색하기
+sql_select_by_dname = '''
+select * from dept_ex
+where upper(dname) like upper(:dept_name)
+'''
+
+# sql_select_by_dname 문장을 실행하고 결과를 출력하는 함수
+def select_dept_by_dname(name):
+    with get_connection() as conn:
+        with conn.cursor() as cursor:
+            try:
+                keyword = f'%{name}%'
+                cursor.execute(sql_select_by_dname, dept_name=keyword)
+                for row in cursor:
+                    print(row)
+            except DatabaseError as e:
+                print(e)
+
+
 if __name__ == '__main__':
-    select_dept()
+    # dept_ex 테이블 전체 검색
+    # select_dept()
+
+    # dept_ex 테이블에서 부서번호로 검색하기
+    dept_no = int(input('부서번호를 입력>>> '))
+    select_dept_by_deptno(dept_no)
+
+    # 부서 이름 검색
+    dept_name = input('부서이름 입력>>> ')
+    select_dept_by_dname(dept_name)
