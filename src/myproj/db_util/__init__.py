4e554e880b8226eb70c6aff0be6cb7b09503b8b3
JakeOh <jake.oh.lecture@gmail.com>
2025-06-18 11:26:29 +0900

    oracledb 패키지를 사용한 모듈 작성

--- src/myproj/db_util/__init__.py
+++ src/myproj/db_util/__init__.py
@@ -1,3 +1,4 @@
 # from .connect import get_connection
 # from .connect import close_connection
-from .connect import get_connection, close_connection
+from src.myproj.db_util.connect import get_connection, close_connection
+from src.myproj.db_util.ddl import create_table, drop_table
