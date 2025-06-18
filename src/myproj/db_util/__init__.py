5cdb89115d25a7e7a194e42cdc3720e3cb5652ab
JakeOh <jake.oh.lecture@gmail.com>
2025-06-18 16:53:35 +0900

    oracledb 패키지를 사용한 dml 모듈 update, delete 쿼리 작성/실행

--- src/myproj/db_util/__init__.py
+++ src/myproj/db_util/__init__.py
@@ -3,3 +3,4 @@
 from src.myproj.db_util.connect import get_connection, close_connection
 from src.myproj.db_util.ddl import create_table, drop_table
 from src.myproj.db_util.dql import select_dept, select_dept_by_deptno, select_dept_by_dname
+from src.myproj.db_util.dml import insert_dept, update_dept, delete_dept
--- src/myproj/db_util/dml.py
+++ src/myproj/db_util/dml.py
@@ -26,12 +26,45 @@ def insert_dept(dept_no, dept_name, location):
                 # 가변길이 keyword arg 방식
                 # cursor.execute(sql_insert, dept_no=dept_no, dept_name=dept_name, location=location)
                 conn.commit()  # DML(insert, update, delte)의 결과를 commit.
+                print('insert 성공')
             except DatabaseError as e:
                 print(e)
 
 # dept_ex 테이블에서 특정 부서 번호의 이름과 위치를 업데이트하는 SQL 문장과 함수를 작성하세요.
+sql_upate = '''
+update dept_ex
+set dname = :dept_name, loc = :location
+where deptno = :dept_no
+'''
+
+def update_dept(dept_no, dept_name, location):
+    with get_connection() as conn:
+        with conn.cursor() as cursor:
+            try:
+                cursor.execute(sql_upate,
+                               dept_no=dept_no,
+                               dept_name=dept_name,
+                               location=location)
+                conn.commit()
+                print('update 성공')
+            except DatabaseError as e:
+                print(e)
 
 # dept_ex 테이블에서 특정 부서 번호의 데이터를 삭제하는 SQL 문장과 함수를 작성하세요.
+sql_delete = '''
+delete from dept_ex
+where deptno = :dept_no
+'''
+
+def delete_dept(dept_no):
+    with get_connection() as conn:
+        with conn.cursor() as cursor:
+            try:
+                cursor.execute(sql_delete, dept_no=dept_no)
+                conn.commit()
+                print('delete 성공')
+            except DatabaseError as e:
+                print(e)
 
 
 if __name__ == '__main__':
@@ -40,3 +73,13 @@ if __name__ == '__main__':
     name = input('부서 이름 입력>>> ')
     loc = input('부서 위치 입력>>> ')
     insert_dept(no, name, loc)
+
+    # update 테스트
+    no = int(input('업데이트할 부서 번호 입력>>> '))
+    name = input('업데이트할 부서 이름 입력>>> ')
+    loc = input('업데이트할 부서 위치 입력>>> ')
+    update_dept(no, name, loc)
+
+    # delete 테스트
+    no = int(input('삭제할 부서 번호 입력>>> '))
+    delete_dept(no)
