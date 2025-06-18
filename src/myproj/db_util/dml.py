d6d50a191a6eeb6d3770e592b279beb4a8651c7f
JakeOh <jake.oh.lecture@gmail.com>
2025-06-18 16:41:16 +0900

    oracledb 패키지를 사용한 dml 모듈 insert 쿼리 작성

--- src/myproj/db_util/dml.py
+++ src/myproj/db_util/dml.py
@@ -1,4 +1,33 @@
+from oracledb import DatabaseError
+
+from src.myproj.db_util import get_connection
+
 # dept_ex 테이블에 데이터(부서번호, 이름, 위치)를 insert하는 SQL 문장과 함수를 작성하세요.
+sql_insert ='''
+insert into dept_ex
+values (:dept_no, :dept_name, :location)
+'''
+
+def insert_dept(dept_no, dept_name, location):
+    # step 1. DB 연결
+    with get_connection() as conn:
+        # step 2. Cursor 객체 생성
+        with conn.cursor() as cursor:
+            # step 3. SQL 실행
+            try:
+                # positional arg 방식 -> list, tuple, dict
+                # result = cursor.execute(sql_insert, (dept_no, dept_name, location))
+                # print('insert result =', result)
+                cursor.execute(sql_insert, {
+                    'dept_no': dept_no,
+                    'dept_name': dept_name,
+                    'location': location
+                })
+                # 가변길이 keyword arg 방식
+                # cursor.execute(sql_insert, dept_no=dept_no, dept_name=dept_name, location=location)
+                conn.commit()  # DML(insert, update, delte)의 결과를 commit.
+            except DatabaseError as e:
+                print(e)
 
 # dept_ex 테이블에서 특정 부서 번호의 이름과 위치를 업데이트하는 SQL 문장과 함수를 작성하세요.
 
@@ -6,5 +35,8 @@
 
 
 if __name__ == '__main__':
-    # 위에서 선언한 함수들을 테스트하세요.
-    pass
+    # insert 테스트
+    no = int(input('부서 번호 입력>>> '))
+    name = input('부서 이름 입력>>> ')
+    loc = input('부서 위치 입력>>> ')
+    insert_dept(no, name, loc)
